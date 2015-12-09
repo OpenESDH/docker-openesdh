74f16a49b545ca247b031ed91f40ca3d5c2556bf
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2015-12-09 10:50:01 +0200

    OPENE-398 OpenE UI modules.

--- init_opene.sh
+++ init_opene.sh
@@ -1,10 +1,12 @@
+npm install
 bower update --allow-root | xargs echo
+gulp all-modules-install
 
 cat > /etc/supervisor/conf.d/openeui.conf << EOF
 [program:openeui]
 priority=20
 directory=/OpeneUI
-command=gulp local
+command=gulp all-modules local
 user=root
 autostart=true
 autorestart=true
