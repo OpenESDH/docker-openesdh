3734724d91bb7918f4a30eadd7db1cc6e1722f70
Arnas Petraitis <arnas.petraitis@visma.com>
2016-03-23 16:03:27 +0200

    adding additional google-docs-repo*.amp to copy into alfresco

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ openeid=`docker inspect -f '{{.Id}}' opene`
 repoupdates=/var/lib/docker/aufs/mnt/$openeid/tmp/opene_updates/opene_repo
 cp addo-webapp*.war $repoupdates
 cp *.amp $repoupdates
+cp ../alfresco-googledocs-repo-3.0.2.amp $repoupdates
 docker exec opene bash /tmp/opene_updates/opene_repo/update_repo.sh
